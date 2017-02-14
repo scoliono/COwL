ca29215cb766cb0778e325bc65606490c39a92d1
Evan Johnston <evanjohnman@gmail.com>
2017-02-13 20:00:00 -0600

    Added functionality to make p tags null out if they exist and are set to default. Added baked-in default attributes because I'm a lazy shit.

--- cowl_jqscripts.js
+++ cowl_jqscripts.js
@@ -8,20 +8,56 @@ $(document).ready(function() {
     });
     $("select").change(function() {
         var pid = $(this).parent().attr("id");
-        console.log(pid);
         var odef = $(this).find("option[name='def']");
         var cval = $(this).find(":selected");
+        pid = pid.split("_");
+        xid = pid.join('-').split(' ').join('');
         if (cval.attr("value") != odef.attr("value")) {
-            pid = pid.split("_");
-            xid = pid.join('-').replace(' ', '');
             if (document.getElementById(xid) == null) {
                 n = document.createElement("p");
                 n.id = xid;
                 document.getElementById('results').appendChild(n);
+//                console.log("made new p tag, added to div")
             }
             var xxid = "#" + xid;
             $(xxid).text("Change dropdown at \"" + pid.join(" => ") + "\" from \"" + odef.text() + "\" to \"" + cval.text() + "\".");
-            console.log(xid)
+//            console.log("set text in p tag")
+        } else {
+                var xxid = "#" + xid;
+                $(xxid).text("");
+                }
+    });
+    $("input[type='range']").on("mousedown mousemove change", function(e) {
+        syncToText($(this).parent().attr("id"));
+        pid = $(this).parent().attr("id").split(" ").join("_").split("_").join("-");
+        if ($(this).val() != $(this).attr("data-default")) {
+            if (document.getElementById(pid) == null) {
+                n = document.createElement("p");
+                n.id = pid;
+                document.getElementById('results').appendChild(n);
+            }
+            var xxid = "#" + pid;
+            $(xxid).text("Change slider at \"" + pid.split("-").join(" => ") + "\" from \"" + $(this).attr("data-default") + "\" to \"" + $(this).val() + "\".");
+        } else {
+                var xxid = "#" + pid;
+                $(xxid).text("");
+        }
+    });
+    $("input[type='number']").on("mousedown mousemove change", function(e) {
+        syncToRange($(this).parent().attr("id"));
+        pid = $(this).parent().attr("id").split(" ").join("_").split("_").join("-");
+        if ($(this).val() != $(this).attr("data-default")) {
+            if (document.getElementById(pid) == null) {
+                n = document.createElement("p");
+                n.id = pid;
+                document.getElementById('results').appendChild(n);
+            }
+            var xxid = "#" + pid;
+            $(xxid).text("Change slider at \"" + pid.split("-").join(" => ") + "\" from \"" + $(this).attr("data-default") + "\" to \"" + $(this).val() + "\".");
+            console.log($(this).val());
+        } else {
+            var xxid = "#" + pid;
+            $(xxid).text("");
         }
     });
 });
